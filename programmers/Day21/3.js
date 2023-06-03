e73d273a1f5f7a5144fe23d6053baa33bf32e136
coachkim <kimsw0825@gmail.com>
2023-06-03 22:08:11 +0900

    삼각형의 완성조건(2)

---
+++ programmers/Day21/3.js
@@ -0,0 +1,18 @@
+function solution(sides) {
+    let count = 0
+    const max = Math.max(...sides)
+    const min = Math.min(...sides)
+    // 만약 기존 요소 중 가장 긴 변이 있다면
+    for(let i = max-min+1 ; i <= max ; i ++) {
+        count++
+    }
+    // 나머지 한 변이 가장 긴 변인 경우
+    for(let i = max+1 ; i < max+min ; i ++) {
+        count++
+    }
+    return count
+}
+// 두 번째 방법
+function solution(sides) {
+    return Math.min(...sides)*2-1
+}
